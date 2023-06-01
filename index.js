b901ec0cbc777644f2372abed2494ff46de61ef2
Marisha <7513541@gmail.com>
2023-06-01 22:58:59 +0300

    tratataataat

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const content = document.getElementById('content')
 const query = new URLSearchParams(window.location.search)
 const bookingId = query.get('q')
 if (!bookingId) {
-	content.innerHTML = 'No Data';
+    content.innerHTML = 'No Data';
 } else {
-	fetch(`${apiUrl}/api/booking/qr/${bookingId}`)
-		.then(res => res.json())
-		.then(data => {
-			content.innerHTML = `
+    fetch(`${apiUrl}/api/booking/qr/${bookingId}`)
+        .then(res => res.json())
+        .then(data => {
+            content.innerHTML = `
 
 			<div class="wrapper">
 				<div class="info">
 					<div class="header">
-						<span>id: ${data.id}</span>
+					<span>id билета: ${bookingId}</span>
 						<h1 class="title">${data.title}</h1>
 					</div>
 					<img class="poster" src="${data.poster_src}" />
@@ -27,9 +27,9 @@ if (!bookingId) {
 				</ul>
 			</div>
 			`
-		})
+        })
 }
 
 function formatPlace(place) {
-	return `<li>${place.side === 'left' ? 'Слева' : 'Справа'}, ряд ${place.row}, место ${place.place}</li>`
+    return `<li>${place.side === 'left' ? 'Слева' : 'Справа'}, ряд ${place.row}, место ${place.place}</li>`
 }
